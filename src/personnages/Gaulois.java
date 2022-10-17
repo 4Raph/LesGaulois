4858502d3569fdb26b7d1f306b49d0893964e4de
osmont raphael <smr4258a@ens.fsi>
2022-10-17 17:45:57 +0200

    TP3 milieu

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,8 +3,9 @@ package personnages;
 
 public class Gaulois {
 	private String nom;
-	private int force;
 	private int effetPotion = 1;
+	private int force, nb_trophees;
+	private Equipement trophees[] = new Equipement[100];
 	
 	public Gaulois(String nom, int force) {
 		super();
@@ -20,19 +21,36 @@ public class Gaulois {
 		System.out.println(prendreParole() + "«" + texte + "»");
 	}
 	
-	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
-	}
+//	private String prendreParole() {
+//		return "Le gaulois " + nom + " : ";
+//	}
 	
-	public void frapper( Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoir de "
-				+ romain.getNom());
-	}
+//	public void frapper( Romain romain) {
+//		System.out.println(nom + " envoie un grand coup dans la mâchoir de "
+//				+ romain.getNom());
+//	}
 	
 	@Override
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
+	
+	private String prendreParole() {
+		String texte = "Le gaulois " + nom + " : ";
+		return texte;
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		Equipement trophees[] = romain.recevoirCoup((force / 3) *
+		effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++,
+				nb_trophees++) {
+			this.trophees[nb_trophees] = trophees[i];
+		}
+		return;
+	}
+
 	
 	public static void main(String[] args) {
 		Gaulois asterix=new Gaulois("Asterix",8);
